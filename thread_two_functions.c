2eaf76de79600500beb7a30ddf7a22811921f6f2
isidrolc <serverlc@prodigy.net.mx>
2020-08-16 21:26:39 -0600

    With windows functions

--- thread_two_functions.c
+++ thread_two_functions.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <ncurses.h>
 
 
 void *function1(void *ptr);
 void *function2(void *ptr);
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+
 int counter1, counter2;
 
 int main()
@@ -56,5 +60,27 @@ void *function2(void *ptr)
 		counter2 = counter2 - 1;
 		sleep(1);
 	}
+}
+
+
+WINDOW *create_newwin(int height, int width, int starty, int startx)
+{
+        WINDOW *local_win;
+
+        local_win = newwin(height, width, starty, startx);
+        box(local_win, 0, 0);
+
+        wrefresh(local_win);
+
+        return local_win;
+
+}
+
+void destroy_win(WINDOW *local_win)
+{
+        wborder(local_win, ' ',' ',' ',' ',' ',' ',' ',' ');
+
+        wrefresh(local_win);
+        delwin(local_win);
 }
 
